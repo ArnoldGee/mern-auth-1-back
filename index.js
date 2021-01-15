05431bdc93857118092e93f1bc8b270171ca63cb
Arnau <agomez1f@gmail.com>
2021-01-15 19:38:18 +0100

    Set up mongoDB

--- index.js
+++ index.js
@@ -6,18 +6,21 @@ const chalkAnimation = require('chalk-animation');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const {MONGODB_CONNECTION_STRING} = process.env
 
 app.use(express.json());
 app.use(cors());
 
 
 
-
-
-
-
-
-
+mongoose.connect(
+  MONGODB_CONNECTION_STRING,
+  {useUnifiedTopology: true, useNewUrlParser: true},
+  (err) => {
+    if (err) throw err;
+    console.log('Connection to MONGODB successful :)'); // Animation starts
+  }
+);
 
 app.listen(PORT, () => {
   chalkAnimation.rainbow('The server has started on port ' + PORT);
